3953b221803163badcc37c8d53f0a50a949a35ad
hws2002 <hwooseok123@gmail.com>
2024-06-18 11:40:50 +0000

    reconstruction

--- Iris_perceptron/data_load.py
+++
@@ -1,9 +0,0 @@
-import os 
-import pandas as pd
-
-s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-
-df = pd.read_csv(s,
-                 header = None,
-                 encoding = 'utf-8')
-df.tail()
