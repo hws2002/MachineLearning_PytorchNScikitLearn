fa6e3b0a1602b09eef52a7acf42cbb7d0801f9d8
hws2002 <hwooseok123@gmail.com>
2024-06-30 13:50:31 +0000

    complete : logistric regression GD

--- classifiers/LogisticRegressionGD/logistic.py
+++ classifiers/LogisticRegressionGD/logistic.py
@@ -3,34 +3,36 @@ import numpy as np
 class LogisticRegressionGD:
     """
         완전 배치 경사 하강법으로 구현한 logistic regression classifier
+        ! 이진 분류에만 적용할 수 있음
     """
     def __init__(self, eta = 0.01, n_iter = 50,random_state = 1):
         self.eta = eta
         self.n_iter = n_iter
-        self.random_state = random_state_state
+        self.random_state = random_state
     
     def fit(self, X, y):
         rng = np.random.default_rng(self.random_state)
         self.w_ = rng.normal(loc = 0.0, scale = 0.01, size = X.shape[1])
-        self.b_ = np.float(0.0)
+        self.b_ = np.float_(0.0)
         self.losses_ = []
         
         # TODO
         for _ in range( self.n_iter):
             net_input = self.net_input(X)
             output = self.activation(net_input)
-            self.w_ = self.eta * 
-            self.b_ = self.eta * 
-            loss = 
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * np.dot(X.T, errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * np.mean(errors)
+            loss = np.mean(-y*np.log(output) - (1-y) * np.log(1-output))
             self.losses_.append(loss)
         
         return self
 
-    def activation(self, X):
+    def activation(self, z):
         """
             로지스틱 시그모이드 활성화 계산
         """
-        return 1.0 / ( 1.0 + np.exp(-X))
+        return 1.0 / ( 1.0 + np.exp( np.clip(-z,-250, 250) ) )
     
     def net_input(self, X):
         return np.dot(X, self.w_) + self.b_
